65db702c41c5ef1ed5b14659f43306c0af50ca3d
Pedro Cabral <pedrocabral747@gmail.com>
2019-01-31 18:12:04 -0200

    final do quarto dia, tree views, calendars, search views, gantt,graph views, kanban

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api, exceptions
-
+from datetime import timedelta
 class odooacademy(models.Model):
     _name = 'odooacademy'
 
@@ -50,12 +50,15 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string = "Number of Seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string = "Instructor", domain = ['|',('instructor','=',True),('category_id.name','ilike',"Teacher")])
     course_id = fields.Many2one('odooacademy.course', ondelete = 'cascade', string = "Course", required = True) 
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
     taken_seats = fields.Float(string = "Taken seats", compute = '_taken_seats')
+    end_date = fields.Date(string = "End Date", store = True, compute = '_get_end_date', inverse='_set_end_date')
+    hours = fields.Float(string = "Duration in hours", compute = '_get_hours', inverse='_set_hours')
+    attendee_count = fields.Integer(string = "Attendees count", compute = '_get_attendees_count', store = True)
 
     @api.depends ('seats', 'attendee_ids')
     def _taken_seats(self): 
@@ -81,12 +84,35 @@ class Session(models.Model):
                     'message': "Increase seats or remove excess attendees",
                 },
             }
-    
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+            
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
+
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendee(self):
         for r in self:
             if r.instructor_id and r.instructor_id in r.attendee_ids:
                 raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+    def _set_hours(self):
+        for r in self:
+            for r in self:
+                r.duration = r.hours / 24
+    
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendee_count = len(r.attendee_ids)
 
 class Partner(models.Model):
     _inherit = 'res.partner'
